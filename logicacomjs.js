780b9a8fb1114c3440ca76b64880f4e86410d0f2
geovannafelixx17 <geovannacurso20@gmail.com>
2025-05-27 16:22:31 -0300

    aprendi if i else

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,6 +1,46 @@
-let x = Number (prompt("Digite um número"))
+/*let x = Number (prompt("Digite um número"))
 console.log(x)
 let salario = 2500
 let doze = 
 let mesestrabalhados = 12 
-console.log(x) ("seu décimo é"+ variavel )
+console.log(x) ("seu décimo é"+ variavel )*/
+
+/*let salario = Number(prompt("Digite seu salário"))
+if (salario <500){
+salario = (15/100*salario)+salario
+alert ("Seu novo salario e:  "+ salario)
+} else if (salario >=500 && salario<=1000){
+    salario = (10/100*salario)+salario
+    alert ("seu novo salario e:" +salario)
+} else {
+    salario = (5/100*salario)+salario
+    alert ("seu novo salario e: " +salario)
+} */
+
+ /*let idade = Number(prompt("digite sua idade"))
+ if (idade <=12) {
+ alert ("recomendavel conteudo infantil")
+ }
+ else if (idade >=12 && idade <=17) {
+    alert("recomendavel conteudo teen")
+ }
+ else if(idade >=18 && idade <=60){
+    alert ("recomendavel conteudo adulto")
+ }
+ else{
+    alert("recomendavel conteudo senior")
+ }*/
+ 
+
+    let pontuacao = Number(prompt("digite sua pontuacao"))
+    if(pontuacao >=1000) {
+        alert(Sua pontuacao mais o bonus e +(pontuacao + 100), "seu nivel e novato")
+    }
+    else if(pontuacao>=1001 && pontuacao <=5000){
+        alert ("sua pontuacao mais o bonus e"+(pontuacao+ 500), "seu nivel e expert")
+    }
+    else if (pontuacao= 5001 && pontuacao <=10000) {
+        alert ("sua pontuacao mais o bonus e " +(pontuacao))
+    }
+
+
