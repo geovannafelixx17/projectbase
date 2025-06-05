a7db17cfe304615d390628e2594b6d3cb79e501c
geovannafelixx17 <geovannacurso20@gmail.com>
2025-06-05 16:51:40 -0300

    aprendendo array

--- logicacomjs.js
+++ logicacomjs.js
@@ -32,7 +32,7 @@ alert ("Seu novo salario e:  "+ salario)
  }*/
  
 
-    let pontuacao = Number(prompt("digite sua pontuacao"))
+   /* let pontuacao = Number(prompt("digite sua pontuacao"))
     if(pontuacao >=1000) {
         alert(Sua pontuacao mais o bonus e +(pontuacao + 100), "seu nivel e novato")
     }
@@ -41,6 +41,71 @@ alert ("Seu novo salario e:  "+ salario)
     }
     else if (pontuacao= 5001 && pontuacao <=10000) {
         alert ("sua pontuacao mais o bonus e " +(pontuacao))
+    }*/
+/*
+let planobasico = 25; 
+let planopremiun = 40;
+let planoultra = 60;
+let letrastresquatro = 5;
+let telasmaisdequatro = 7.50;
+
+
+let tipodeassinatura = prompt("digite o nome da assinatura que deseja")
+let quantitelas = prompt ("digite quantas telas voce deseja querido cliente:")
+if(tipoassinatura == "basico"){
+    if (quantitelas>=3 && quantitelas <=4){
+        alert ("seu plano e R$  " + (valordobasico+letrastresquatro))
+    } else if (quantitelas >= && quantitelas <=4){
+        alert ("o seu palono sera R$ " + (valordobasico+letrastresquatro))
+    } else {
+        alert ("o seu plano sera R$ "+valordobasico)
+    } 
+
+}
+
+else if (tipoassinatura == "premiun") {
+    if (quantitelas>=3 && quantitelas <=4){
+        alert ("seu plano e R$  " + (valordopremiun+letrastresquatro))
+    } else if (quantitelas >= && quantitelas <=4)}
+        alert ("o seu palono sera R$ " + (valordopremiun+letrastresquatro))
+    } else {
+        alert ("o seu plano sera R$ "+valordopremiun)
+    
     }
+
+else if (tipoassinatura == "ultra")
+    if (quantitelas>=3 && quantitelas <=4){
+        alert ("seu plano e R$  " + (valordoultra+letrastresquatro))
+    } else if (quantitelas >= && quantitelas <=4){
+        alert ("o seu palono sera R$ " + (valordoultra+letrastresquatro))
+    } else {
+        alert ("o seu plano sera R$ "+valordoultra)
+    
+    } */
+
+       /* let daysOfweek = ["Caio","Samuel",80,40,10]; // Iniciando uma Array
+        console.log(daysOfweek[1]) // Ver por indice
+        console.log(daysOfweek.length) // ver quantos itens tem nesse Array
+        let listdiver = ["Caio",1,2.5,"Escola",false]
+        console.log(listdiver[3])
+        let nomedoaluno = prompt ("digite o nome do novo aluno")
+        daysOfweek.unshift(nomedoaluno)
+        daysOfweek.push("Pedro","alisson","arthur")
+        daysOfweek.push("joao")
+        daysOfweek.pop()
+        daysOfweek.shift()
+        daysOfweek.sort()
+        daysOfweek. reverse()
+        alert("Os alunos atualmente matriculados sao"+daysOfweek)
+        alert(daysOfweek.lastIndexOf(geovanna))*/
+
+
+        
+
+
+    
+
+
+
 
 
